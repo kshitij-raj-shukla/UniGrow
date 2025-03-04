1db91e609589ef57e960b7bf006d4e4b0cb97a74
kshitij-raj-shukla <122507762+kshitij-raj-shukla@users.noreply.github.com>
2025-03-04 22:11:33 +0530

    Delete uniBackend directory

--- uniBackend/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { processPayment } = require("../controllers/paymentController");
-const { protect } = require("../middleware/authMiddleware");
-
-const router = express.Router();
-
-router.post("/", protect, processPayment);
-
-module.exports = router;
